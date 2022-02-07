9c6de4c2b4c38b8617240f64cf86244c4168b37e
James Cliff <me@jamescliff.com>
2022-02-07 12:21:11 -0500

    feat: icons

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import "../styles/globals.css";
 
 const title = "Orbital";
 const url = "https://orbital-webapp.vercel.app/";
+const image = "/orbital-icon.png";
 const description = "Drexel UXID Senior Project Team";
 const author = "The Orbital Team";
 
@@ -31,33 +32,21 @@ function MyApp({ Component, pageProps }) {
         <meta name="og:title" content={title} />
         <meta name="og:type" content="site" />
         <meta name="og:url" content={url} />
-        <meta name="og:image" content={"/icons/share.png"} />
+        <meta name="og:image" content={image} />
         <meta name="og:site_name" content={title} />
         <meta name="og:description" content={description} />
 
-        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
-        <link
-          rel="apple-touch-icon"
-          sizes="16x16"
-          href="/icons/favicon-16x16.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="32x32"
-          href="/icons/favicon-32x32.png"
-        />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="32x32" href="/favicon-32x32.png" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/icons/apple-touch-icon.png"
+          href="/apple-touch-icon.png"
         />
         <link rel="manifest" href="/manifest.json" />
-        <link
-          rel="mask-icon"
-          color="#000000"
-          href="/icons/safari-pinned-tab.svg"
-        />
-        <link rel="apple-touch-startup-image" href="/startup.png" />
+        <link rel="mask-icon" color="#000000" href="/safari-pinned-tab.svg" />
+        {/* <link rel="apple-touch-startup-image" href="/startup.png" /> */}
 
         {/* Meta Tags for HTML pages on Mobile */}
         {/* <meta name="format-detection" content="telephone=yes"/>
@@ -67,7 +56,7 @@ function MyApp({ Component, pageProps }) {
           content="width=device-width, minimum-scale=1, initial-scale=1.0"
         />
         <meta name="theme-color" content="#000" />
-        <link rel="shortcut icon" href="/icons/favicon.ico" />
+        <link rel="shortcut icon" href="/favicon.ico" />
 
         {/* 
       Twitter Summary card
