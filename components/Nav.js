589ae57807a0d5e68f26a26b797e7ab8b1b352f4
James Cliff <me@jamescliff.com>
2022-02-27 15:49:57 -0500

    fix: click

--- components/Nav.js
+++ components/Nav.js
@@ -2,7 +2,7 @@ import Link from "./Link";
 
 export default function Nav() {
   return (
-    <nav className="absolute top-0 w-full p-2 m-auto z-50">
+    <nav className="absolute top-0 p-2 m-auto z-50">
       <ul className="flex w-full list-none">
         <li className="block mr-4">
           <Link label="Create" url="/" />
