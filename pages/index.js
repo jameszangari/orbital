b9caa364c5342c65ecfc655f58bd81dbcb03215e
James Cliff <me@jamescliff.com>
2022-04-02 17:08:40 -0400

    feat: updates

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,30 @@
 import Head from "next/head";
-import React, { useState } from "react";
-import { useRouter } from "next/router";
+import React from "react";
+import Image from "next/image";
 import Link from "../components/Link";
+import Icon from "../components/Icon";
 
 export default function Index({ posts }) {
-  const router = useRouter();
-
+  const description =
+    "Orbital is an interactive art exhibit where visitors can create a custom planet and add it to our collaborative solar system and see it projected in real-time. Through the individual contributions of our visitors, our solar system will be filled with unique and vibrant planets for all to see.";
   return (
     <>
       <Head>
         <title>Orbital | Welcome</title>
       </Head>
       {/* TODO password protect this page */}
-      <h1 className="text-center pt-24 pb-8 text-3xl max-w-3xl m-auto">
-        Welcome to Orbital!
-      </h1>
-      <div className="py-4 max-w-3xl m-auto">
-        <p>
-          Enim magna amet cillum exercitation nulla aliqua ullamco minim duis.
-          Reprehenderit cupidatat occaecat do sint ad dolor esse consequat est
-          nulla. Aliqua nostrud aliqua incididunt. Aliqua dolor ad anim non
-          irure laborum enim irure id quis elit. Dolor duis consequat esse elit.
-          Reprehenderit reprehenderit eu ad sit tempor. Cupidatat pariatur duis
-          occaecat sint aliqua laboris elit aliqua pariatur labore consequat
-          incididunt. Consequat aliqua elit adipisicing tempor in consectetur
-          minim pariatur magna est nisi consequat enim irure. Eiusmod incididunt
-          pariatur dolor quis ea duis duis sint do commodo veniam id in tempor
-          aliquip. Elit laborum qui duis adipisicing aliqua anim consectetur.
-        </p>
-        <Link
-          variant={"link"}
-          url={"/create"}
-          label={"Create Your Planet!"}
-          className={"mt-8"}
-        />
+      <div className="py-4 max-w-3xl m-auto grid place-items-center h-screen">
+        <div className="flex flex-col justify-center">
+          <Icon />
+          <h1 className="text-center pb-8 pt-24 text-3xl">Welcome!</h1>
+          <p>{description}</p>
+          <Link
+            variant={"link"}
+            url={"/create"}
+            label={"Create Your Planet!"}
+            className={"mt-8"}
+          />
+        </div>
       </div>
       {/* {posts
         ? posts.map((planet, i) => {
