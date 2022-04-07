fd5dd2a137f2fa889b41cd25c7a6e13318d44623
James Cliff <me@jamescliff.com>
2022-04-07 11:20:26 -0400

    fix: fonts

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,8 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
             href="https://fonts.googleapis.com/css2?family=Krona+One&family=Space+Grotesk:wght@400;500;700&display=swap"
             rel="stylesheet"
