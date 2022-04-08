ad6085cb610e7a5588e1d20e769a64ed3dc3ff05
James Cliff <me@jamescliff.com>
2022-04-08 11:02:27 -0400

    fix: cleanup

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import React from "react";
-import Image from "next/image";
 import Link from "../components/Link";
 import Icon from "../components/Icon";
 
@@ -12,7 +11,6 @@ export default function Index({ posts }) {
       <Head>
         <title>Orbital | Welcome</title>
       </Head>
-      {/* TODO password protect this page */}
       <div className="py-4 max-w-3xl m-auto grid place-items-center h-screen">
         <div className="flex flex-col justify-center">
           <Icon />
@@ -26,77 +24,6 @@ export default function Index({ posts }) {
           />
         </div>
       </div>
-      {/* {posts
-        ? posts.map((planet, i) => {
-            {
-              console.log(planet);
-            }
-            return (
-              <div key={i} className="py-4 max-w-3xl m-auto">
-                <p className="flex flex-row w-full">
-                  <span className="w-1/2">ID: </span>
-                  <span className="w-1/2 text-right">{planet._id}</span>
-                </p>
-                <p className="flex flex-row w-full">
-                  <span className="w-1/2">Created: </span>
-                  <span className="w-1/2 text-right">{planet.createdAt}</span>
-                </p>
-                <p className="flex flex-row w-full">
-                  <span className="w-1/2">type: </span>
-                  <span className="w-1/2 text-right">{planet.pType}</span>
-                </p>
-                <p className="flex flex-row w-full">
-                  <span className="w-1/2">size: </span>
-                  <span className="w-1/2 text-right">{planet.pSize}</span>
-                </p>
-                <p className="flex flex-row w-full">
-                  <span className="w-1/2">core: </span>
-                  <span className="w-1/2 text-right">
-                    {planet.pCoreColor.hex}
-                  </span>
-                </p>
-                <p className="flex flex-row w-full">
-                  <span className="w-1/2">atmos: </span>
-                  <span className="w-1/2 text-right">
-                    {planet.pAtmosColor.hex}
-                  </span>
-                </p>
-                <p className="flex flex-row w-full">
-                  <span className="w-1/2">core texture: </span>
-                  <span className="w-1/2 text-right">
-                    {planet.pCoreTexture}
-                  </span>
-                </p>
-                <p className="flex flex-row w-full">
-                  <span className="w-1/2">cloud texture: </span>
-                  <span className="w-1/2 text-right">
-                    {planet.pCloudTexture}
-                  </span>
-                </p>
-                <p className="flex flex-row w-full">
-                  <span className="w-1/2">cloud alpha: </span>
-                  <span className="w-1/2 text-right">{planet.pCloudAlpha}</span>
-                </p>
-              </div>
-            );
-          })
-        : null} */}
     </>
   );
-}
-export async function getServerSideProps(ctx) {
-  // get the current environment
-  let dev = process.env.NODE_ENV !== "production";
-  let { DEV_URL, PROD_URL } = process.env;
-
-  // request posts from api
-  let response = await fetch(`${dev ? DEV_URL : PROD_URL}/api/posts`);
-  // extract the data
-  let data = await response.json();
-
-  return {
-    props: {
-      posts: data["message"],
-    },
-  };
 }
