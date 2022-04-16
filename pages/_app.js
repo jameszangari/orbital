c68cdabb6d260b15ea4ae7d9bf92dfe0030bb7ef
James Cliff <me@jamescliff.com>
2022-04-16 14:44:09 -0400

    fix: strip down branch

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,54 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Head from "next/head";
+
+const seo = {
+  title: "Orbital: UXID Senior Project",
+  url: "https://orbital-webapp.vercel.app/",
+  image: "/orbital-cover.jpg",
+  description:
+    "Orbital is a covid-safe interactive art installation allowing for the collaboration of visitors to manipulate and create a one-of-a-kind experience in real time.",
+  author: "The Orbital Team",
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>{seo.title}</title>
+        <meta property="og:title" content={seo.title} />
+        <meta property="description" content={seo.description} />
+        <meta property="og:description" content={seo.description} />
+        <meta name="keywords" content={seo.keywords} />
+        <meta property="og:image" content={seo.image} />
+        <meta name="msapplication-TileColor" content="#000" />
+        <meta name="theme-color" content="#000" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/meta/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/meta/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/meta/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/meta/site.webmanifest" />
+        <link rel="mask-icon" href="/meta/safari-pinned-tab.svg" color="#000" />
+        <link rel="icon" href={seo.image} />
+      </Head>
+      <main className="overflow-x-hidden">
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,89 +1,15 @@
 import Head from "next/head";
+import Logo from "./Logo";
 
-export default function Home({ posts }) {
-  console.log(posts);
+export default function Home() {
   return (
     <div>
       <Head>
-        <title>Orbital | Observe</title>
+        <title>Orbital | Coming Soon</title>
       </Head>
-
-      <main>
-        {posts
-          ? posts.map((planet, i) => {
-              {
-                console.log(planet);
-              }
-              return (
-                <div key={i} className="py-4 max-w-3xl m-auto">
-                  <p className="flex flex-row w-full">
-                    <span className="w-1/2">ID: </span>
-                    <span className="w-1/2 text-right">{planet._id}</span>
-                  </p>
-                  <p className="flex flex-row w-full">
-                    <span className="w-1/2">Created: </span>
-                    <span className="w-1/2 text-right">{planet.createdAt}</span>
-                  </p>
-                  <p className="flex flex-row w-full">
-                    <span className="w-1/2">type: </span>
-                    <span className="w-1/2 text-right">{planet.pType}</span>
-                  </p>
-                  <p className="flex flex-row w-full">
-                    <span className="w-1/2">size: </span>
-                    <span className="w-1/2 text-right">{planet.pSize}</span>
-                  </p>
-                  <p className="flex flex-row w-full">
-                    <span className="w-1/2">core: </span>
-                    <span className="w-1/2 text-right">
-                      {planet.pCoreColor.hex}
-                    </span>
-                  </p>
-                  <p className="flex flex-row w-full">
-                    <span className="w-1/2">atmos: </span>
-                    <span className="w-1/2 text-right">
-                      {planet.pAtmosColor.hex}
-                    </span>
-                  </p>
-                  <p className="flex flex-row w-full">
-                    <span className="w-1/2">core texture: </span>
-                    <span className="w-1/2 text-right">
-                      {planet.pCoreTexture}
-                    </span>
-                  </p>
-                  <p className="flex flex-row w-full">
-                    <span className="w-1/2">cloud texture: </span>
-                    <span className="w-1/2 text-right">
-                      {planet.pCloudTexture}
-                    </span>
-                  </p>
-                  <p className="flex flex-row w-full">
-                    <span className="w-1/2">cloud alpha: </span>
-                    <span className="w-1/2 text-right">
-                      {planet.pCloudAlpha}
-                    </span>
-                  </p>
-                </div>
-              );
-            })
-          : null}
+      <main className="h-screen w-screen flex flex-col justify-center items-center p-8">
+        <Logo className="w-96 max-w-full" />
       </main>
     </div>
   );
-}
-
-export async function getServerSideProps(ctx) {
-  // get the current environment
-  let dev = process.env.NODE_ENV !== "production";
-  let { DEV_URL, PROD_URL } = process.env;
-
-  // request posts from api
-  let response = await fetch(`${dev ? DEV_URL : PROD_URL}/api/posts`);
-  // extract the data
-  let data = await response.json();
-
-  return {
-    props: {
-      posts: data["message"],
-    },
-  };
 }
