d6f77ceacfd3b167d707e50fd16f7468124b3f7c
James Cliff <me@jamescliff.com>
2022-04-25 09:05:04 -0400

    fix: landscape on index

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import React from "react";
 import Link from "../components/Link";
+import Background from "../components/Background";
+import RotateIcon from "../components/RotateIcon";
 import { motion } from "framer-motion";
 
 export default function Index({ posts }) {
@@ -11,7 +13,7 @@ export default function Index({ posts }) {
       <Head>
         <title>Orbital | Welcome</title>
       </Head>
-      <motion.div>
+      <motion.div className="portrait:hidden">
         <motion.div className="py-4 max-w-3xl m-auto grid place-items-center mx-4">
           <motion.div className="flex flex-col justify-center">
             <motion.h1 className="pb-8 pt-24 text-3xl">Welcome!</motion.h1>
@@ -23,6 +25,15 @@ export default function Index({ posts }) {
               className={"mt-8 w-max"}
             />
           </motion.div>
+        </motion.div>
+      </motion.div>
+      <motion.div className="landscape:hidden z-50 h-screen w-full flex flex-col justify-center items-center bg-black bg-opacity-75">
+        <motion.div className="absolute z-50 flex flex-col justify-center items-center mx-4">
+          <RotateIcon className={"block w-24 h-24 mt-8"} />
+          <motion.h3 className="text-xl font-secondary uppercase p-8">
+            Please rotate your device.
+          </motion.h3>
+          <Background />
         </motion.div>
       </motion.div>
     </>
