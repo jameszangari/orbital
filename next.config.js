c87e129a767fcd297512d2483a87ae220d59d09c
James Cliff <me@jamescliff.com>
2022-04-28 09:47:14 -0400

    feat: pwa

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
-module.exports = {
-  // reactStrictMode: true,
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
+
+module.exports = withPWA({
   swcMinify: true,
   images: {
     minimumCacheTTL: 60,
     loader: "custom",
   },
-};
+  pwa: {
+    dest: "public",
+    runtimeCaching,
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
+});
